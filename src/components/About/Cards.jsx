2d466f56c63a68c0457566b5dd63f6c945906a37
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2023-04-18 22:24:10 +0300

    inlineStyleCard

--- src/components/About/Cards.jsx
+++ src/components/About/Cards.jsx
@@ -5,6 +5,8 @@ import {
   CardHead,
   Description,
   Img1,
+  ImgCard,
+  SixCard,
 } from './Cards.styled';
 import c1 from '..//..//images/Cards/C1.png';
 import c2 from '..//..//images/Cards/c2.png';
@@ -21,63 +23,79 @@ export default function Card() {
   return (
     <CardSection>
       <CardHead>
-        <Card1>
+        <Card1 style={{ background: '#FFE3D8' }}>
           <Description>
             Наші умови максимально прості не передбачають додаткових платежів та
             комісій
           </Description>
           <Img1 src={c1} alt="пазл"></Img1>
         </Card1>
-        <Card1>
+        <Card1 style={{ background: '#DDEFE8' }}>
           <Description>
             Кілька варіантів отримання і повернення позики. Ми надаємо вибір.
             Просто вирішіть, як вам зручніше
           </Description>
           <Img1 src={c2} alt="хмарка"></Img1>
         </Card1>
-        <Card2>
+        <Card2 style={{ background: '#FFF2CB' }}>
           <Description>
             Після надсилання SMS на номер сервісу потрібна сума протягом доби
             опиниться на банківському рахунку або стане доступною практично
             моментально через Google-гаманець або в будь-якому з 9 000 пунктів
             обслуговування
           </Description>
-          <Img1 src={c3} alt="гроші"></Img1>
-          <Img1 src={c31} alt="телефон"></Img1>
+          <ImgCard style={{ marginTop: '75px' }}>
+            <Img1 src={c3} alt="гроші"></Img1>
+            <Img1 src={c31} alt="телефон"></Img1>
+          </ImgCard>
         </Card2>
-        <Card1>
+        <Card1 style={{ background: '#E6E6FC' }}>
           <Description>
             Середній час обробки заявки - всього 7 хвилин.
           </Description>
           <Img1 src={c4} alt="годинник"></Img1>
         </Card1>
-        <Card1>
+        <Card1 style={{ background: '#FFE3D8' }}>
           <Description>
             Можливість продовжувати позику необмежену кількість разів
           </Description>
           <Img1 src={c5} alt="календар"></Img1>
         </Card1>
-        <Card2>
-          <Description>
-            День зарплати ще не скоро? Ми можемо зачекати! Просто сплатіть
-            відсотки за позикою, і він автоматично триватиме ще на 21 день
-          </Description>
-          <Img1 src={c6} alt="монетка"></Img1>
-          <Img1 src={c61} alt="календарик"></Img1>
+        <Card2 style={{ display: 'flex', background: '#DDEFE8' }}>
+          <SixCard>
+            <Description>
+              День зарплати ще не скоро? Ми можемо зачекати! Просто сплатіть
+              відсотки за позикою, і він автоматично триватиме ще на 21 день
+            </Description>
+            <Img1
+              src={c6}
+              alt="монетка"
+              style={{ marginTop: '75px', marginLeft: '20px' }}
+            ></Img1>
+          </SixCard>
+          <Img1
+            src={c61}
+            alt="календарик"
+            style={{ width: '500px', height: '500px', padding: '26px' }}
+          ></Img1>
         </Card2>
-        <Card1>
+        <Card1 style={{ background: '#FFF2CB' }}>
           <Description>
             ТОВ МКК «Креатив» працює дистанційно по всій Україні, тому отримання
             позики можливе будь-коли в будь-якому місці.
           </Description>
           <Img1 src={c7} alt="світ"></Img1>
         </Card1>
-        <Card1>
+        <Card1 style={{ background: '#E6E6FC' }}>
           <Description>
             Максимально проста схема отримання та повернення позики без застави
             поручителів
           </Description>
-          <Img1 src={c8} alt="окей"></Img1>
+          <Img1
+            src={c8}
+            alt="окей"
+            style={{ marginTop: '75px', width: '192px' }}
+          ></Img1>
         </Card1>
       </CardHead>
     </CardSection>
--- src/components/About/Cards.styled.jsx
+++ src/components/About/Cards.styled.jsx
@@ -14,7 +14,6 @@ export const Card1 = styled.li`
   width: 552px;
   height: 552px;
   border-radius: 16px;
-  background: #ddefe8;
   margin: 12px;
 
   display: flex;
@@ -35,10 +34,18 @@ export const Description = styled.p`
   font-weight: 500;
   font-size: 24px;
   line-height: 32px;
-
+  padding: 40px;
   color: #3f3e42;
+  text-align: center;
 `;
 export const Img1 = styled.img`
-  width: 352px;
-  height: 336px;
+  width: 337px;
+  height: 300px;
+  // margin-top: 40px;
+  border-radius: 16px;
 `;
+export const ImgCard = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+export const SixCard = styled.span``;
