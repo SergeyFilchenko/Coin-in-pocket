3047a98cc7f1928ed4d11cc3461f9c60b2329961
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2023-04-24 12:41:15 +0300

    change

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,31 +1,40 @@
+import {
+  FooterSection,
+  Container,
+  ListFirst,
+  ListSecond,
+  ListTheard,
+  ListFour,
+} from './Footer.styled';
+
 export default function Footer() {
   return (
-    <div>
-      <div>
-        <ul>
+    <FooterSection>
+      <Container>
+        <ListFirst>
           <h2>Монета в кишені</h2>
           <p>+38(067) 115-50-95</p>
-          <p>info@monetavkisheni.com</p>
+          <p>info@MonetaVkisheni.com</p>
           <button>Особистий кабінет</button>
-        </ul>
-        <ul>
+        </ListFirst>
+        <ListSecond>
           <li>Про компанію</li>
           <li>Послуги</li>
           <li>Тарифи</li>
           <li>Способи погашення</li>
-        </ul>
-        <ul>
+        </ListSecond>
+        <ListTheard>
           <li>Правила надання позик</li>
           <li>Свідоцтво МФО</li>
           <li>Політика конфіденційності</li>
-        </ul>
-        <ul>
+        </ListTheard>
+        <ListFour>
           <li>Зразок заяви на мікропозику</li>
           <li>Список учасників</li>
           <li>Відповіді на запитання</li>
           <li>Контакти</li>
-        </ul>
-      </div>
-    </div>
+        </ListFour>
+      </Container>
+    </FooterSection>
   );
 }
--- src/components/Footer/Footer.styled.jsx
+++ src/components/Footer/Footer.styled.jsx
@@ -1,1 +1,7 @@
 import styled from 'styled-components';
+export const FooterSection = styled.div``;
+export const Container = styled.div``;
+export const ListFirst = styled.ul``;
+export const ListSecond = styled.ul``;
+export const ListTheard = styled.ul``;
+export const ListFour = styled.ul``;
