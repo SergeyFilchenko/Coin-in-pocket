80f32cf6659060e92087034b90892b59c9390510
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2023-04-24 13:44:25 +0300

    Footer+

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -18,28 +18,53 @@ export default function Footer() {
             <Logo src={frame} alt="coin" />
             Монета в кишені
           </Head>
-          <Item>+38(067) 115-50-95</Item>
-          <Item>info@MonetaVkisheni.com</Item>
+          <Item>
+            <a href=".#">+38(067) 115-50-95</a>
+          </Item>
+          <Item>
+            <a href=".#">info@MonetaVkisheni.com</a>
+          </Item>
           <Btn>Особистий кабінет</Btn>
         </List>
         <List>
           <Item>
             <a href=".#">Про компанію</a>
           </Item>
-          <Item>Послуги</Item>
-          <Item>Тарифи</Item>
-          <Item>Способи погашення</Item>
+          <Item>
+            <a href=".#">Послуги</a>
+          </Item>
+          <Item>
+            <a href=".#">Тарифи</a>
+          </Item>
+          <Item>
+            <a href=".#">Способи погашення</a>
+          </Item>
         </List>
         <List>
-          <Item>Правила надання позик</Item>
-          <Item>Свідоцтво МФО</Item>
-          <Item>Політика конфіденційності</Item>
+          <Item>
+            <a href=".#">Правила надання позик</a>
+          </Item>
+          <Item>
+            {' '}
+            <a href=".#">Свідоцтво МФО</a>
+          </Item>
+          <Item>
+            <a href=".#">Політика конфіденційності</a>
+          </Item>
         </List>
         <List>
-          <Item>Зразок заяви на мікропозику</Item>
-          <Item>Список учасників</Item>
-          <Item>Відповіді на запитання</Item>
-          <Item>Контакти</Item>
+          <Item>
+            <a href=".#">Зразок заяви на мікропозику</a>
+          </Item>
+          <Item>
+            <a href=".#">Список учасників</a>
+          </Item>
+          <Item>
+            <a href=".#">Відповіді на запитання</a>
+          </Item>
+          <Item>
+            <a href=".#">Контакти</a>
+          </Item>
         </List>
       </Container>
     </FooterSection>
