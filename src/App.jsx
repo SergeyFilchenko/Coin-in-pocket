945b152b39694274e9435886b480d63aa5e81295
SergeyFilchenko <romantik908@gmail.com>
2023-06-15 21:15:26 +0300

    start main section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './index.css';
 import Header from './components/Header/Header';
 // import About from 'components/About/About';
 import Footer from 'components/Footer/Footer';
+import Main from 'components/Main/Main';
 
 export const App = () => {
   return (
     <>
       <Header />
       {/* <About /> */}
+      <Main/>
       <Footer />
     </>
   );
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,5 @@
+export default function Main (){
+    return (
+       <div><h2>HELLO</h2></div>
+    )
+}
