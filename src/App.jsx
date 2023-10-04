7b86f4c5ea896873caf2a0d053823e670d72869f
SergeyFilchenko <romantik908@gmail.com>
2023-10-04 21:07:50 +0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { Route, Routes } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 import './index.css';
 import Header from './components/Header/Header';
 // import About from 'components/About/About';
@@ -9,10 +9,12 @@ export const App = () => {
   return (
     <>
       <Header />
-
-      {/* <About /> */}
       <Main />
-      <Footer />
+        <Footer />
+      {/* <About /> */}
+      <Routes>
+        
+      </Routes>
     </>
   );
 };
